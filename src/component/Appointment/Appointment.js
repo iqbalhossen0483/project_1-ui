01d4e0c115a4b602a6eb3aa8976983410a46ea5c
md-rakib420 <rayhan.senbag.bd@gmail.com>
2021-10-29 18:49:37 +0600

    ok

--- src/component/Appointment/Appointment.js
+++ src/component/Appointment/Appointment.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Appointment = () => {
     return (
-        <div className="h-80 w-2/6 mx-auto mt-10 mb-32 ">
+        <div className="my-20 overflow-hidden mx-5 md:w-2/4 md:mx-auto lg:w-4/12 xl:w-1/4">
             <h3 className="text-3xl font-semibold text-center mb-3">Book an appointment</h3>
             <div
                 className="w-full py-10 flex flex-col justify-center"
@@ -14,7 +14,7 @@ const Appointment = () => {
                     <input className="block ml-4 mt-4 py-2 px-4 rounded focus:outline-none" type="number" placeholder="Phone number" />
                     <input className="block ml-4 mt-4 py-2 px-4 rounded focus:outline-none" type="email" placeholder="Email address" />
                     <input className="block ml-4 mt-4 py-2 px-4 rounded focus:outline-none" type="date" placeholder="dd/mm/yy" />
-                    <input className="block ml-4 mt-4 py-2 px-4 rounded " type="submit" value="submit" />
+                    <input className="block ml-4 mt-4 py-2 px-4 rounded" type="submit" value="submit" />
                 </form>
             </div>
         </div>
